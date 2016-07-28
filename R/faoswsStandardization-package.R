7dd791580a26b5db955dc8ed908074c33fb31c34
Sebastian <sebastian-c@users.noreply.github.com>
2016-07-28 14:43:18 +0200

    Added more package documentation

--- R/faoswsStandardization-package.R
+++ R/faoswsStandardization-package.R
@@ -1,4 +1,6 @@
 #' Package for standardization of Food Balance Sheets
+#'
+#' This package contains support functions for the standardization process. The process contains balancing as well (contained in the faoswsBalancing package). The main function \code{standardizationWrapper} controls the flow of the whole process.
 #' 
 #' @docType package
 #' @name faoswsStandardization-package
