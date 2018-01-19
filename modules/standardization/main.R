66075c48308d18e07ac9a8f3b45400990f408f23
CristinaMuschitiello <muschitiello@gmail.com>
2018-01-19 09:20:37 +0100

    Starting point for New Version developement. PLUGIN

--- modules/standardization/main.R
+++ modules/standardization/main.R
@@ -41,6 +41,9 @@ if (CheckDebug()) {
     token = PARAMS[["token"]]
   )
   
+
+  batchnumber = 000 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! SET IT   
+
 } else {
   message("Running on server, no need to call GetTestEnvironment...")
   
