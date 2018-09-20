734ad881d5787d3c8fa85ccaa8ff6502bb47b404
Nathan Wanner <Nathan.Wanner@fao.org>
2018-09-20 09:39:22 +0200

    Change to new feed table

--- modules/FullstandardizationAndBalancing/main.R
+++ modules/FullstandardizationAndBalancing/main.R
@@ -289,8 +289,10 @@ data=convertSugarCodes(data)
 ############### CREATE THE COLUMN "OFFICIAL" #################
 ##############################################################
 flagValidTable = ReadDatatable("valid_flags")
-Feed_Items = ReadDatatable("feed_classification")
-Feed_Items = filter(Feed_Items,classification=="feedOnly",!is.na(classification))
+Feed_Items = ReadDatatable("feed_items_2018")
+Feed_Items = rename(Feed_Items,classification=feed_class)
+Feed_Items$classification = "feedOnly"
+# Feed_Items = filter(Feed_Items,classification=="feedOnly",!is.na(classification))
 data=left_join(data,flagValidTable,by=c("flagObservationStatus","flagMethod"))%>%
   data.table
 
