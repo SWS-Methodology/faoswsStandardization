4db38e583217396c896728e41de3a92a4a1a03af
DelBello <Carlo.DelBello@fao.org>
2018-10-09 15:22:06 +0200

    This module computes caloric aggregates at sua balanced level

---
+++ modules/aggregateSuaBal/main.R
@@ -0,0 +1,186 @@
+##'
+##' **Author: Carlo Del Bello**
+##'
+##' **Description:**
+##'
+##' This module is designed to aggregate des figures at SUA balanced level
+##' 
+##' 
+##' **Inputs:**
+##'
+##' * sua balanced
+##' 
+##'' **Output:**
+##'
+##' * csv file attached in mail
+
+
+
+
+
+## load the library
+library(faosws)
+library(data.table)
+library(faoswsUtil)
+library(sendmailR)
+library(dplyr)
+library(faoswsUtil)
+#library(faoswsStandardization)
+library(faoswsFlag)
+
+
+## set up for the test environment and parameters
+R_SWS_SHARE_PATH = Sys.getenv("R_SWS_SHARE_PATH")
+
+if(CheckDebug()){
+  message("Not on server, so setting up environment...")
+  
+  library(faoswsModules)
+  SETT <- ReadSettings("modules/aggregateSuaBal/sws.yml")
+  
+  R_SWS_SHARE_PATH <- SETT[["share"]]  
+  ## Get SWS Parameters
+  SetClientFiles(dir = SETT[["certdir"]])
+  GetTestEnvironment(
+    baseUrl = SETT[["server"]],
+    token = SETT[["token"]]
+  )
+}
+
+#startYear = as.numeric(swsContext.computationParams$startYear)
+startYear = as.numeric(2010)
+
+#endYear = as.numeric(swsContext.computationParams$endYear)
+endYear = as.numeric(2016)
+#geoM49 = swsContext.computationParams$geom49
+stopifnot(startYear <= endYear)
+yearVals = startYear:endYear
+
+##' Get data configuration and session
+sessionKey = swsContext.datasets[[1]]
+
+sessionCountries =
+  getQueryKey("geographicAreaM49", sessionKey)
+
+geoKeys = GetCodeList(domain = "agriculture", dataset = "aproduction",
+                      dimension = "geographicAreaM49")[type == "country", code]
+
+top48FBSCountries = c(4,24,50,68,104,120,140,144,148,1248,170,178,218,320,
+                      324,332,356,360,368,384,404,116,408,450,454,484,508,
+                      524,562,566,586,604,608,716,646,686,762,834,764,800,
+                      854,704,231,887,894,760,862,860)
+
+# top48FBSCountries<-as.character(top48FBSCountries)
+# 
+# selectedCountries = setdiff(geoKeys,top48FBSCountries) #229
+# 
+
+
+# ##Select the countries based on the user input parameter
+selectedGEOCode =sessionCountries
+
+
+
+
+
+#########################################
+##### Pull from SUA balanced data #####
+#########################################
+
+message("Pulling SUA balanced Data")
+
+#take geo keys
+geoDim = Dimension(name = "geographicAreaM49", keys = selectedGEOCode)
+
+#Define element dimension. These elements are needed to calculate net supply (production + net trade)
+
+eleKeys = GetCodeList(domain = "suafbs", dataset = "sua_balanced", "measuredElementSuaFbs")
+eleKeys <-eleKeys[, code]
+
+eleDim <- Dimension(name = "measuredElementSuaFbs", keys = eleKeys)
+
+
+#Define item dimension
+
+itemKeys = GetCodeList(domain = "suafbs", dataset = "sua_balanced", "measuredItemFbsSua")
+itemKeys = itemKeys[, code]
+
+itemDim <- Dimension(name = "measuredItemFbsSua", keys = itemKeys)
+
+
+# Define time dimension
+
+timeDim <- Dimension(name = "timePointYears", keys = as.character(yearVals))
+
+#Define the key to pull SUA data
+key = DatasetKey(domain = "suafbs", dataset = "sua_balanced", dimensions = list(
+  geographicAreaM49 = geoDim,
+  measuredElementSuaFbs = eleDim,
+  measuredItemFbsSua = itemDim,
+  timePointYears = timeDim
+))
+
+
+
+sua_balanced_data = GetData(key)
+# 
+ kcalData <- subset(sua_balanced_data, measuredElementSuaFbs %in% c("664"))
+ 
+ kcalData[,6:7]<-NULL
+ 
+ kcalData<-kcalData %>% group_by(geographicAreaM49,timePointYears) %>%  mutate(totDES=sum(Value,na.rm = T))
+
+ kcalData2 = melt.data.table(setDT(kcalData),id.vars = c("geographicAreaM49","measuredItemFbsSua","timePointYears"),
+                             measure.vars = c("Value","totDES"))
+ setnames(kcalData2, "variable", "Aggregation")
+ setnames(kcalData2, "value", "Value")
+ 
+ kcalData2[,Aggregation:=ifelse(Aggregation=="Value","Item","GRAND TOTAL")]
+ 
+ grandTotal=subset(kcalData2,Aggregation=="GRAND TOTAL")
+ grandTotal[,measuredItemFbsSua:="S2901"]
+ grandTotal=unique(grandTotal)
+ grandTotal=nameData("suafbs","fbs_balanced_",grandTotal)
+items=nameData("suafbs","sua_balanced",subset(kcalData2,Aggregation=="Item"))
+ 
+ kcalDataWide = dcast.data.table(setDT(items),geographicAreaM49+geographicAreaM49_description+
+                                   measuredItemFbsSua+measuredItemFbsSua_description~timePointYears , 
+                                 fun.aggregate = NULL,value.var = "Value")
+ GTWide = dcast.data.table(setDT(grandTotal),geographicAreaM49+geographicAreaM49_description+
+                             measuredItemFbsSua+measuredItemFbsSua_description~timePointYears , fun.aggregate = NULL,value.var = "Value")
+ 
+ toSend=rbind(GTWide,kcalDataWide)
+ 
+# fbsTree<-ReadDatatable("fbs_tree")
+# fbsCodes<-GetCodeList(domain = "suafbs", dataset = "fbs_balanced_", "measuredItemFbsSua")
+# fbsCodes=subset(fbsCodes,grepl("^S",code))
+# fbsCodes=subset(fbsCodes,grepl("[[:upper:]]\\s",description))
+# fbsCodes$code<-gsub("^S","",fbsCodes$code)
+# 
+# fbsTree <- merge(fbsTree,fbsCodes,by.x = "id1",by.y = "code")
+# 
+# 
+# kcalData_id <- merge(kcalData,fbsTree,by.x = "measuredItemFbsSua",by.y = "item_sua_fbs")
+# 
+# totalDes=kcalData_id[,totDES:=sum(Value, na.rm = T), by=c("geographicAreaM49,timePointYears,id1")]
+# 
+# #kcalData<-kcalData[,totDES:=sum(Value, na.rm = T), by=c("geographicAreaM49,timePointYears,id1")]
+# #kcalData<-kcalData[,totDES:=sum(Value, na.rm = T), by=c("geographicAreaM49,timePointYears,id2")]
+# 
+ # kcalData2 = melt.data.table(totalDES,id.vars = c("geographicAreaM49","measuredElementSuaFbs","measuredItemFbsSua","timePointYears","Value","totDES"),
+ #                           measure.vars = "totDES")
+# kcalData2 = dcast.data.table(setDT(kcalData2),geographicAreaM49+measuredItemFbsSua~timePointYears + totDES, fun.aggregate = NULL,value.var = "Value")
+# 
+# #kcalData<-nameData("suafbs","fbs_balanced",sua_balanced_data)
+# 
+# 
+# #sua_balanced_data$measuredItemFbsSua <- paste0("'",sua_balanced_data$measuredItemFbsSua,sep = "")
+# 
+
+
+bodySuaBALAggregation= paste("The Email contains the aggregated caloric intake at sua balanced level",
+                          sep='\n')
+
+sendMailAttachment(toSend,"SuaBalAggregation",bodySuaBALAggregation)
+
+
