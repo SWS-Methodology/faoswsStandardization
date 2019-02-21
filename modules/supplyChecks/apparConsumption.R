6c23c9034fc7510c4e53319b4d3cc08e1c16fd2e
Christian Mongeau <chrMongeau@users.noreply.github.com>
2019-02-21 14:46:44 +0100

    Use data.table consistently; other small fixes

--- modules/supplyChecks/apparConsumption.R
+++ modules/supplyChecks/apparConsumption.R
@@ -36,19 +36,18 @@ if(CheckDebug()){
 #endYear = as.numeric(swsContext.computationParams$endYear)
 #endYear = as.numeric(2016)
 
-startYear = 2010
-endYear = 2017 # TODO: parameterise
-geoM49 = swsContext.computationParams$geom49
+startYear <- 2010
+endYear <- 2017 # TODO: parameterise
+geoM49 <- swsContext.computationParams$geom49
 stopifnot(startYear <= endYear)
-yearVals = startYear:endYear
+yearVals <- startYear:endYear
 
 ##' Get data configuration and session
-sessionKey = swsContext.datasets[[1]]
+sessionKey <- swsContext.datasets[[1]]
 
-sessionCountries =
-  getQueryKey("geographicAreaM49", sessionKey)
+sessionCountries <- getQueryKey("geographicAreaM49", sessionKey)
 
-geoKeys = GetCodeList(domain = "agriculture", dataset = "aproduction",
+geoKeys <- GetCodeList(domain = "agriculture", dataset = "aproduction",
                       dimension = "geographicAreaM49")[type == "country", code]
 
 top48FBSCountries = c(4,24,50,68,104,120,140,144,148,1248,170,178,218,320,
@@ -61,136 +60,112 @@ top48FBSCountries = c(4,24,50,68,104,120,140,144,148,1248,170,178,218,320,
 # selectedCountries = setdiff(geoKeys,top48FBSCountries) #229
 # 
 
+commodity_table <- ReadDatatable("fbs_commodity_definitions")
+
+stopifnot(nrow(commodity_table) > 0)
 
 # ##Select the countries based on the user input parameter
-selectedGEOCode =
-  switch(geoM49,
-         "session" = sessionCountries,
-         "all" = geoKeys)
-
-
-
-
+selectedGEOCode <-
+  switch(
+    geoM49,
+    "session" = sessionCountries,
+    "all"     = geoKeys
+  )
 
 #########################################
 ##### Pull from SUA unbalanced data #####
 #########################################
 
-message("Pulling SUA Unbalanced Data")
+#message("Pulling SUA Unbalanced Data")
 
-#take geo keys
-geoDim = Dimension(name = "geographicAreaM49", keys = selectedGEOCode)
+# Take geo keys
 
-#Define element dimension. These elements are needed to calculate net supply (production + net trade)
+geoDim <- Dimension(name = "geographicAreaM49", keys = selectedGEOCode)
 
-eleKeys = GetCodeList(domain = "suafbs", dataset = "sua_unbalanced", "measuredElementSuaFbs")
-eleKeys <-eleKeys[, code]
+# Define element dimension. These elements are needed to calculate net supply (production + net trade)
+
+eleKeys <- GetCodeList(domain = "suafbs", dataset = "sua_unbalanced", "measuredElementSuaFbs")$code
 
 eleDim <- Dimension(name = "measuredElementSuaFbs", keys = eleKeys)
 
+# Define item dimension
 
-#Define item dimension
-
-itemKeys = GetCodeList(domain = "suafbs", dataset = "sua_unbalanced", "measuredItemFbsSua")
-itemKeys = itemKeys[, code]
+itemKeys <- GetCodeList(domain = "suafbs", dataset = "sua_unbalanced", "measuredItemFbsSua")$code
 
 itemDim <- Dimension(name = "measuredItemFbsSua", keys = itemKeys)
-
 
 # Define time dimension
 
 timeDim <- Dimension(name = "timePointYears", keys = as.character(yearVals))
 
-#Define the key to pull SUA data
-key = DatasetKey(domain = "suafbs", dataset = "sua_unbalanced", dimensions = list(
+# Define the key to pull SUA data
+
+key <- DatasetKey(domain = "suafbs", dataset = "sua_unbalanced", dimensions = list(
   geographicAreaM49 = geoDim,
   measuredElementSuaFbs = eleDim,
   measuredItemFbsSua = itemDim,
   timePointYears = timeDim
 ))
 
+sua_data_full <- GetData(key)
+
+sua_data <-
+  sua_data_full[
+    measuredElementSuaFbs %in% c("5510","5610","5910","664")
+  ][
+    is.na(Value),
+    Value := 0
+  ][,
+    oldprod := sum(Value[as.numeric(timePointYears) < 2014 & measuredElementSuaFbs == "5510"]),
+    .(geographicAreaM49, measuredItemFbsSua, measuredElementSuaFbs)
+  ][,
+    drop := any(oldprod > 0),
+    .(geographicAreaM49, measuredItemFbsSua)
+  ][
+    drop == FALSE & timePointYears > 2013
+  ][,
+    c("flagObservationStatus","flagMethod") := NULL
+  ]
 
 
-sua_data_full = GetData(key)
+sua_data <-
+  dcast.data.table(
+    sua_data,
+    geographicAreaM49 + measuredItemFbsSua + timePointYears ~ measuredElementSuaFbs,
+    value.var = "Value",
+    fill = 0
+  )
 
+setnames(sua_data, c("5510","5610","5910"), c("production","imports", "exports"))
 
-sua_data <- subset(sua_data_full, measuredElementSuaFbs %in% c("5510","5610","5910","664"))
-
-sua_data[is.na(Value), Value := 0]
-
-
-sua_data<- sua_data %>% group_by(geographicAreaM49, measuredItemFbsSua, measuredElementSuaFbs) %>% 
-   mutate(oldprod=sum(Value[timePointYears<2014 & measuredElementSuaFbs=="5510"]))
-
-sua_data<- sua_data %>% group_by(geographicAreaM49, measuredItemFbsSua) %>% mutate(drop=any(oldprod>0))
-sua_data=as.data.table(sua_data)
-
-sua_data <- subset(sua_data, drop==F)
-
-
-sua_data[, c("flagObservationStatus","flagMethod") := NULL]
-
-sua_data <- subset(sua_data, timePointYears>2013)
-
-
-sua_data <- dcast.data.table(sua_data, geographicAreaM49 + measuredItemFbsSua +
-                          timePointYears ~ measuredElementSuaFbs, value.var = "Value")
-
-
-
-setnames(sua_data, c("5510","5610","5910"),c("production","imports", "exports"))
-
-
-sua_data[is.na(imports), imports := 0]
-sua_data[is.na(exports), exports := 0]
-sua_data[is.na(production), production := 0]
-
-
-sua_data[,supply := production+imports-exports]
-
+sua_data[, supply := production + imports - exports]
 
 ## livestock cpc to exclude
-'%!in%' <- function(x,y)!('%in%'(x,y))
-
-livestock<- c("02111","02112","02121.01","02122","02123","02131","02132","02133","02140","02151","02152","02153","02154","02191","02194",
+livestock <- c("02111","02112","02121.01","02122","02123","02131","02132","02133","02140","02151","02152","02153","02154","02191","02194",
               "02196","02199.10","02199.20")
 
+primary <- commodity_table[primary_commodity == "X", cpc]
 
-#print apparent consumption
-
-
-commDef=ReadDatatable("fbs_commodity_definitions")
-primary=commDef$cpc[commDef[,primary_commodity=="X"]]
-
-apparentConsumption <- copy (sua_data)
-
-
-apparentConsumption <- subset(apparentConsumption, timePointYears %in% c(2014:2016))
-
-
-apparentConsumption <- subset(apparentConsumption, measuredItemFbsSua %!in% livestock)
-
-apparentConsumption <- subset(apparentConsumption, supply < -10 )
-apparentConsumption <- subset(apparentConsumption, measuredItemFbsSua %in% primary )
-
+apparentConsumption <-
+  sua_data[
+    timePointYears %in% 2014:endYear &
+      !(measuredItemFbsSua %in% livestock) &
+      supply < -10 &
+      measuredItemFbsSua %in% primary
+  ]
 
 #apparentConsumption<- apparentConsumption[,c("geographicAreaM49","measuredItemFbsSua","timePointYears"),with=FALSE]
 
 setcolorder(apparentConsumption, c("geographicAreaM49", "measuredItemFbsSua", "timePointYears", "supply", "production", "imports", "exports"))
 
-apparentConsumption <-nameData("suafbs","sua_unbalanced",apparentConsumption)
-
-apparentConsumption[,timePointYears_description := NULL]
+apparentConsumption <- nameData("suafbs", "sua_unbalanced", apparentConsumption, except = "timePointYears")
 
 #### send mail
-bodyApparent= paste("The Email contains a list of negative apparent consumptions at sua unbalanced level. Negative values higher than -1000 have been filtered out",
-                      sep='\n')
+bodyApparent <- "The Email contains a list of negative apparent consumptions at sua unbalanced level. Negative values higher than -1000 have been filtered out"
 
-sendMailAttachment(apparentConsumption,"apparentConsumption",bodyApparent)
-
+sendMailAttachment(apparentConsumption, "apparentConsumption", bodyApparent)
 
 #apparent consumption is the list of commodities,countries and years where supply is negative.
 
 
 print("Module ran successfully. Check your e-mail.")
-
