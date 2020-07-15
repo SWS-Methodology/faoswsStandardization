4aeb17677048e2e6310cd860ce5ca4c39e23cc19
Amsata <amsata_niang@yahoo.fr>
2020-07-15 10:08:10 +0200

    Adding the element 665: Food supply(g:capita:day) in SUA balanced.
    Correcting outlier correction

--- modules/SUA_bal_compilation/main.R
+++ modules/SUA_bal_compilation/main.R
@@ -4066,13 +4066,13 @@ if (FIX_OUTLIERS == TRUE) {
 
   dout[
     timePointYears >= 2014 & (is.na(mean_ratio) | is.nan(mean_ratio) | is.infinite(mean_ratio)),
-    mean_ratio := mean(ratio[timePointYears >= 2014], na.rm = TRUE),
+    mean_ratio := median(ratio[timePointYears >= 2014], na.rm = TRUE),
     by = c('geographicAreaM49', 'measuredItemSuaFbs', 'measuredElementSuaFbs')
   ]
 
   dout[
     timePointYears >= 2014 & (is.na(Meanold) | is.nan(Meanold) | is.infinite(Meanold)),
-    Meanold := mean(Value[timePointYears >= 2014], na.rm = TRUE),
+    Meanold := median(Value[timePointYears >= 2014], na.rm = TRUE),
     by = c('geographicAreaM49', 'measuredItemSuaFbs', 'measuredElementSuaFbs')
   ]
 
@@ -5371,6 +5371,13 @@ calories_per_capita <-
     by = c('geographicAreaM49', 'timePointYears')
   )
 
+data_for_foodGrams<-calories_per_capita[measuredElementSuaFbs=="664"]
+data_for_foodGrams[,Value:=(food*1000000)/(365*population*1000)]
+data_for_foodGrams[, c("food", "nutrient", "population") := NULL]
+data_for_foodGrams[,measuredElementSuaFbs:="665"]
+data_for_foodGrams[, Protected := FALSE]
+
+
 calories_per_capita_total<-copy(calories_per_capita)
 
 calories_per_capita[, Value := food * nutrient / population / 365 * 10]
@@ -5394,7 +5401,7 @@ calories_per_capita_total[measuredElementSuaFbs=="674",
 calories_per_capita_total[measuredElementSuaFbs=="684",
                           measuredElementSuaFbs:="281"]
 
-calories_per_capita<-rbind(calories_per_capita,calories_per_capita_total)
+calories_per_capita<-rbind(calories_per_capita,calories_per_capita_total,data_for_foodGrams)
 
 standData <-
   rbind(
